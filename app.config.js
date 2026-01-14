67ef60804fe4fd2d896d1e63d8b6aa97774a69fa
GarciaPiedraEdwinLeonardo <egarciap2306@alumno.ipn.mx>
2026-01-13 22:53:25 -0600

    Apk

--- app.config.js
+++ app.config.js
@@ -2,32 +2,46 @@ require("dotenv").config();
 
 export default {
   expo: {
+    owner: "copypastebatiz",
+
     name: "TheHeartCloudMovil",
     slug: "theheartcloudmovil",
+
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
+
     splash: {
       image: "./assets/splash-icon.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff",
     },
+
     assetBundlePatterns: ["**/*"],
+
     ios: {
       supportsTablet: true,
     },
+
     android: {
+      package: "com.copypastebatiz.theheartcloudmovil",
       softwareKeyboardLayoutMode: "resize",
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
     },
+
     web: {
       favicon: "./assets/favicon.png",
     },
+
     extra: {
+      eas: {
+        projectId: "9ae51d52-ec30-400d-8f30-1a1b7b78794e",
+      },
+
       firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
       firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
@@ -36,11 +50,13 @@ export default {
         process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
       firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
       firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+
       cloudName: process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME,
       uploadPresetCedules:
         process.env.EXPO_PUBLIC_CLOUDINARY_UPLOAD_PRESET_CEDULAS,
       uploadPresetProfile:
         process.env.EXPO_PUBLIC_CLOUDINARY_UPLOAD_PRESET_PERFIL,
+
       backendUrl: process.env.EXPO_PUBLIC_BACKEND_URL,
     },
   },
